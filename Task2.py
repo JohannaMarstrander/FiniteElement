5052ad7df6191089b3685fe046444bf8e187d919
JohannaMarstrander <johannamarstrander@hotmail.com>
2020-09-29 10:30:06 +0200

    modified plot function for grids

--- Task2.py
+++ Task2.py
@@ -12,9 +12,9 @@ def PlotMesh(N):
     
     fig, ax = plt.subplots()
     for el in tri:
-        ax.plot(p[el, 0], p[el, 1], "ro-")
-        ax.plot(p[el[[2,0]], 0], p[el[[2,0]], 1], "ro-")
+        ax.plot(p[el, 0], p[el, 1],  "ro-",color = "black")
+        ax.plot(p[el[[2,0]], 0], p[el[[2,0]], 1], "ro-",color = "black")
     return ax
     #print("p:", p, "\n tri:", tri, "\n edge:", edge)
 
-ax = PlotMesh(30)
+ax = PlotMesh(4)
