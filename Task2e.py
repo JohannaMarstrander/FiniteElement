67c8e08b36c69ed3fa3deca27099846ffa5628a3
Nanna Berre <nanna_berre@hotmail.com>
2020-09-30 15:20:27 +0200

    working on homogenous

--- Task2e.py
+++ Task2e.py
@@ -7,15 +7,35 @@ Created on Tue Sep 29 10:35:29 2020
 from getdisc import GetDisc
 import numpy as np
 from Task1 import quadrature2D
+#from createF import createF
 
-def createA(N, Nq):
+def lin(x,y,c,g):
+    return (c[0]+x*c[1]+y*c[2])*g(x,y)
+
+
+def createF(g,N,Nq):
+    p, tri, edge = GetDisc(N)
+    F = np.zeros(N)
+    T = np.ones((3, 3))
+    for el in tri:
+        p1,p2,p3=p[el[0]],p[el[1]],p[el[2]]
+        T[:,1:]=p1,p2,p3
+        C = np.linalg.solve(T, alpha)
+        for i in range(0,2):
+            F[el[i]]+=quadrature2D(p1,p2,p3,Nq,lin,C[i],g)
+    return F
+
+def createAandF(g,N, Nq):
     #Returns A as well as list of corners of edge lines for incorporating BCs
     p, tri, edge = GetDisc(N)
     A = np.zeros((len(p), len(p)))
+    F = np.zeros(N)
+
     
     for el in tri: #for each element
         #Find coefficients of H_alpha^k
         C = np.hstack((np.array([[1],[1],[1]]), p[el]))
+        p1, p2, p3 = p[el[0]], p[el[1]], p[el[2]]
         b1, b2, b3 = np.array([1,0,0]), np.array([0,1,0]), np.array([0,0,1])
         coeff = np.array([np.linalg.solve(C, b1), np.linalg.solve(C, b2), np.linalg.solve(C, b3)])
 
@@ -25,17 +45,35 @@ def createA(N, Nq):
             for j in range(len(el)):
                 c = np.dot(coeff[i,1:], coeff[j,1:])
                 A[el[i],el[j]] += quadrature2D(p[el[0]],p[el[1]],p[el[2]],Nq,func, c)
-    return A, edge
+            F[el[i]] += quadrature2D(p1, p2, p3, Nq, lin, coeff[i], g)
+    return A,F, edge,p
 
-A, edge = createA(5,4)
+#A, edge = createA(5,4)
 
-print("A:",A)
+#print("A:",A)
 
-def homogeneousDirichlet(N, Nq):
-    A, edge = createA(N, Nq)
+def homogeneousDirichlet(N, Nq,f):
+    #F = createF(f,N,Nq)
+    A,F, edge,p = createAandF(f,N, Nq)
     nodes = np.unique(edge)
-    epsilon = 1e-10
+    F[nodes]=0
+    epsilon = 1e-16
     A[nodes, nodes] = 1/epsilon
-    return A
+    u=np.linalg.solve(A,F)
+    return u,p
 
-print("A_eps:", homogeneousDirichlet(5,4))
+def f(x,y):
+    return -8*np.pi*np.cos(2*np.pi*(x**2+y**2))+16*np.pi**2*(x**2+y**2)*np.sin(2*np.pi*(x**2+y**2))
+
+def u(x,y):
+    return np.sin(2*np.pi*(x**2+y**2))
+
+
+
+u_num,p=homogeneousDirichlet(100,4,f)
+
+u_ex=u(p[:,0],p[:,1])
+print(u_num)
+print(u_ex)
+
+print(u_num-u_ex)
